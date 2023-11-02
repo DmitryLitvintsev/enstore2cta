ee5475486a3417b7b8a016465053d4d78c95479d
Dmitry Litvintsev <litvinse@fnal.gov>
2023-11-02 18:51:29 -0500

    correct setup syntax

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     readme = f.read()
 
-    with open("LICENSE") as f:
-        license = f.read()
+with open("LICENSE") as f:
+    license = f.read()
 
-    setup(
-        name="enstore2cta",
-        version="0.1.0",
-        description="Enstore to CTA migration script",
-        long_description=readme,
-        author="Dmitry Litvintsev",
-        url="https://github.com/DmitryLitvintsev/enstore2cta"
-        license=license,
-        packages=find_packages(exclude=('tests', 'docs'))
-        )
+setup(
+    name="enstore2cta",
+    version="0.1.0",
+    description="Enstore to CTA migration script",
+    long_description=readme,
+    author="Dmitry Litvintsev",
+    url="https://github.com/DmitryLitvintsev/enstore2cta",
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+    )
